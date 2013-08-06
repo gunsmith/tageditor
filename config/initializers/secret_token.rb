7e64ba8017fef96ff58b34c1d7ff66f9e292b507
Akio Kobayashi <kobayashi@xatu.(none)>
2013-08-06 16:43:21 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tageditor::Application.config.secret_key_base = 'd411bcc8ef29d5a4a1915c8e94e36853506d70eb9d3cb9ab48be06d7ca83cf7b9315b4fd01987dfc599ba8971d0a3babb96fed34216941422ea4f49c80c0f5b9'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Tageditor::Application.config.session_store :cookie_store, key: '_tageditor_session'
